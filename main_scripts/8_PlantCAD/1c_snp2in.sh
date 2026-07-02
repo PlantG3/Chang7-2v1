8bbcb560e124edb0562b40244edba8420a6099f0
liu3zhenlab <liu3zhen@ksu.edu>
2026-07-01 23:13:42 -0500

    update

--- main_scripts/8_PlantCAD/1c_snp2in.sh
+++ main_scripts/8_PlantCAD/1c_snp2in.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cut -f 2-5 1i_Ch72_EMS.v1.txt > 1i_Ch72_EMS.v1.4cols.txt
 
+# the format of the input file: 1i_Ch72_EMS.v1.4cols.txt
+# can be referred to example.snp.input.txt
+ 
 perl snp2in.pl \
   --fas /homes/liu3zhen/references/Ch7-2v1/genome/Ch7-2v1.fasta \
   --snp 1i_Ch72_EMS.v1.4cols.txt \
--- main_scripts/8_PlantCAD/2c_split.sh
+++ main_scripts/8_PlantCAD/2c_split.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir 2o_split
 cd 2o_split
-sh ~/scripts/regular/hsplit ../1o_Ch72_EMS.v1.512bp.context.txt 3000 EMS
+sh hsplit ../1o_Ch72_EMS.v1.512bp.context.txt 3000 EMS
 cd ..
 
